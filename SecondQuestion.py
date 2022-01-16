1758b573d9c3d537e2bda240b1d3e7eed0f05601
Zyad Al-Azazi <60244865+zyadalazazi@users.noreply.github.com>
2022-01-16 22:11:45 +0200

    Update SecondQuestion.py

--- SecondQuestion.py
+++ SecondQuestion.py
@@ -1,16 +1,20 @@
-# Reading the input from the user
-num = input("Enter a number that is greater than 99:")
 
-# Making sure the input is a number greater than 99
-valid = False
-while not valid:
-    if num.isnumeric():
-        if int(num) > 99:
-            valid = True
+def print_3digit():
+    # Reading the input from the user
+    num = input("Enter a number that is greater than 99:")
+
+    # Making sure the input is a number greater than 99
+    valid = False
+    while not valid:
+        if num.isnumeric():
+            if int(num) > 99:
+                valid = True
+            else:
+                num = input("Please enter a number greater than 99:")
         else:
-            num = input("Please enter a number greater than 99:")
-    else:
-        num = input("Please enter a number not a word nor a character:")
+            num = input("Please enter a number not a word nor a character:")
 
-# Printing the third digit from the number
-print("The third digit for the given number {0} is: {1}".format(num, num[-3]))
+    # Printing the third digit from the number
+    print("The third digit for the given number {0} is: {1}".format(num, num[-3]))
+    
+print_3digit()
